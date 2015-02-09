92b145b6614e3ebfa5c0c24b50b6f6ad867e1aa0
danilo0792 <danilo_sp06@hotmail.com>
2015-02-09 15:58:21 -0200

    Lendo resultado do banco de dados

--- database.php
+++ database.php
@@ -1,8 +1,34 @@
 <?php
 // Executa Querys
 function DBExecute($query){
-$conexao = DBConnect();
-$resultado = mysqli_query($conexao, $query) or die(mysqli_error());
-DBClose($conexao);
-return $resultado;
+  $conexao = DBConnect();
+  $resultado = mysqli_query($conexao, $query) or die(mysqli_error());
+  DBClose($conexao);
+  return $resultado;
+}
+
+// Grava registros
+function DBCreate($table, array $data){
+  $data = DBEscape($data);
+  $fields = implode(',', array_keys($data));
+  $values = "'".implode("','", $data)."'";
+  $query = "insert into {$table}({$fields}) values ({$values})";
+  return DBExecute($query);
+}
+
+// Ler Registros
+function DBRead($table, $params = null, $fields = '*'){
+  $params = $params ? $params = " {$params}" : null;
+  $query = "select {$fields} from {$table}{$params}";
+  $resultado = DBExecute($query);
+  $clientes = array();
+
+  if (!mysqli_num_rows($resultado)) {
+    return false;
+  }else{
+    while($cliente = mysqli_fetch_assoc($resultado)){
+      array_push($clientes, $cliente);
+    }
+    return $clientes;
+  }
 }
--- index.php
+++ index.php
@@ -10,8 +10,18 @@ require_once('config.php');
 require_once('connection.php');
 require_once('database.php');
 
-$query = "insert into clientes (nome) values ('Danilo')";
-var_dump(DBExecute($query));
+// $cliente = array(
+//   'nome'    => 'Danilo',
+//   'email'   => 'danilo_sp06@hotmail.com',
+//   'idade'   => 22,
+//   'status'  => 1
+//   );
+
+// $grava = DBCreate('clientes',$cliente);
+
+$clientes = DBRead('clientes', null, 'nome,email');
+var_dump($clientes);
+
 
 ?>
 </body>
